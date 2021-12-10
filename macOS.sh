83abfd314ed14d26e23f1395f759584e5ca7f32e
Even Zhou <even.y.zhou@outlook.com>
2021-12-10 21:25:10 +0800

    update Dec 10, 2021 21:25

---
+++ macOS.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+/usr/bin/xcode-select --install
+/usr/sbin/softwareupdate --install-rosetta
+
+printf "Please manually download MacPorts in the following link:\n"
+/usr/bin/open https://github.com/macports/macports-base/releases/
+/usr/bin/read -p "Press enter to continue"
+
+sudo /opt/local/bin/port selfupdate
+sudo /opt/local/bin/port install bash bash-completion
+sudo /opt/local/bin/port install vim
+sudo /opt/local/bin/port install git
+sudo /opt/local/bin/port install wget curl tree
+sudo /opt/local/bin/port install openssh
+sudo /opt/local/bin/port install python310 py310-pip py310-setuptools py310-autopep8 py310-codestyle
+sudo /opt/local/bin/port install podman gvisor-tap-vsock qemu
+
+sudo /opt/local/bin/port select --set python python310
+sudo /opt/local/bin/port select --set python3 python310
+sudo /opt/local/bin/port select --set pip pip310
+sudo /opt/local/bin/port select --set pip3 pip310
+sudo /opt/local/bin/port select --set autopep8 py310-autopep8
+sudo /opt/local/bin/port select --set pycodestyle pycodestyle-py310
