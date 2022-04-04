45895b4ad83edfe47231132af8979e9faa8e8f07
Even Zhou <even.y.zhou@outlook.com>
2022-04-04 16:52:03 -0400

    update Apr 04, 2022 16:52

--- macOS.sh
+++ macOS.sh
@@ -8,8 +8,9 @@ printf "Please manually download MacPorts in the following link:\n"
 
 sudo /opt/local/bin/port selfupdate
 sudo /opt/local/bin/port -N install bash bash-completion vim git wget curl tree openssh podman
-sudo /opt/local/bin/port -N install python310 py310-pip py310-setuptools py310-autopep8 py310-codestyle
+sudo /opt/local/bin/port -N install python38 py38-pip py38-setuptools py38-autopep8 py38-codestyle
 sudo /opt/local/bin/port -N install python39 py39-pip py39-setuptools py39-autopep8 py39-codestyle
+sudo /opt/local/bin/port -N install python310 py310-pip py310-setuptools py310-autopep8 py310-codestyle
 
 sudo /opt/local/bin/port select --set python python310
 sudo /opt/local/bin/port select --set python3 python310
